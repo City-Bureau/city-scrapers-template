4222974b32e1609a5d47570aef68253833cac653
Daniel A.T. Warren <dan@warrennetworkservices.com>
2019-11-22 19:25:13 -0500

    flake8 fixes for airport

--- tests/test_alle_airport.py
+++ tests/test_alle_airport.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from os.path import dirname, join
 
-import pytest
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
@@ -31,6 +30,7 @@ freezer.stop()
 Uncomment below
 """
 
+
 def test_title():
     assert parsed_items[0]["title"] == "Allegheny County Airport Authority Board Meeting"
 
@@ -42,8 +42,9 @@ def test_description():
 def test_start():
     assert parsed_items[0]["start"] == datetime(2019, 1, 18, 11, 30, 0)
 
+
 def test_end():
-    assert parsed_items[0]["end"] == None
+    assert parsed_items[0]["end"] is None
 
 
 def test_time_notes():
@@ -61,7 +62,7 @@ def test_time_notes():
 def test_location():
     assert parsed_items[0]["location"] == {
         "name": "Pittsburgh International Airport",
-        "address": "Conference Room A, 4th Flr Mezzanine,"\
+        "address": "Conference Room A, 4th Flr Mezzanine,"
                    " Landside Terminal, Pittsburgh International Airport"
     }
 
@@ -75,7 +76,8 @@ def test_links():
       "href": "https://www.flypittsburgh.com/about-us/leadership",
       "title": "Leadership - Pittsburgh International Airport"
     }]
-    
+
+
 def test_classification():
     assert parsed_items[0]["classification"] == NOT_CLASSIFIED
 
