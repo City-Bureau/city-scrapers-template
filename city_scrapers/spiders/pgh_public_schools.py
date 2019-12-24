b14fd10e04983feedfe6ceef35cf1599b8a72640
Mishugana <Matt@gmail.comddd>
2019-12-24 10:52:24 +0000

    Line too long error throwing flake8

--- city_scrapers/spiders/pgh_public_schools.py
+++ city_scrapers/spiders/pgh_public_schools.py
@@ -35,7 +35,10 @@ class PghPublicSchoolsSpider(CityScrapersSpider):
         modules = "&ModuleInstanceFilter="
 
         # this line is to filter just school board meetings.
-        category_filters = "0-49-40-21-16-4-3-44-39-1-57-43-64-65-58-62-28-25-52-50-55-38-59-17-13-51-56-8-63-53-37-54-7-47-46-33-60-10-19-66-61-48-34-45-41-42-"
+        category_filters = (
+            "0-49-40-21-16-4-3-44-39-1-57-43-64-65-58-62-28-25-"
+            "52-50-55-38-59-17-13-51-56-8-63-53-37-54-7-47-46-33-60-10-19-66-61-48-34-45-41-42-"
+        )
 
         category = "&CategoryFilter={}".format(category_filters)
         dbstream = "&IsDBStreamAndShowAll=true"
