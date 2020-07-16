965ee6a6ee55d7e0a7ae27f2409dfdfa27d45cb1
pjsier <pjsier@gmail.com>
2020-07-16 08:09:31 -0500

    Update isort, style checks

--- city_scrapers/settings/prod.py
+++ city_scrapers/settings/prod.py
@@ -1,10 +1,12 @@
-from .base import *
+import os
 
-USER_AGENT = "City Scrapers [production mode]. Learn more and say hello at https://citybureau.org/city-scrapers"
+from .base import *  # noqa
 
-# Uncomment one of the following DiffPipeline classes to enable a diff pipeline that will
-# deduplicate output UIDs based on City Scrapers IDs and list any meetings in the future which
-# no longer appear in scraped results as cancelled.
+USER_AGENT = "City Scrapers [production mode]. Learn more and say hello at https://citybureau.org/city-scrapers"  # noqa
+
+# Uncomment one of the following DiffPipeline classes to enable a diff pipeline that
+# will deduplicate output UIDs based on City Scrapers IDs and list any meetings in the
+# future which no longer appear in scraped results as cancelled.
 
 # Configure item pipelines
 ITEM_PIPELINES = {
@@ -14,12 +16,12 @@ ITEM_PIPELINES = {
     "city_scrapers_core.pipelines.OpenCivicDataPipeline": 400,
 }
 
-# Uncomment one of the StatusExtension classes to write an SVG badge of each scraper's status to
-# Azure or S3 after each time it's run.
+# Uncomment one of the StatusExtension classes to write an SVG badge of each scraper's
+# status to Azure or S3 after each time it's run.
 
-# By default, this will write to the same bucket or container as the feed export, but this can be
-# configured by adding a value in the CITY_SCRAPERS_STATUS_BUCKET or CITY_SCRAPERS_STATUS_CONTAINER
-# for S3 and Azure respectively.
+# By default, this will write to the same bucket or container as the feed export, but
+# this can be configured by adding a value in the CITY_SCRAPERS_STATUS_BUCKET or
+# CITY_SCRAPERS_STATUS_CONTAINER for S3 and Azure respectively.
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 
@@ -37,8 +39,8 @@ FEED_EXPORTERS = {
 
 FEED_FORMAT = "jsonlines"
 
-# Uncomment S3 or Azure to write scraper results to static file storage as newline-delimited JSON
-# files made up of JSCalendar events following the meeting schema.
+# Uncomment S3 or Azure to write scraper results to static file storage as
+# newline-delimited JSON files made up of OCD events following the meeting schema.
 
 FEED_STORAGES = {
     # "s3": "scrapy.extensions.feedexport.S3FeedStorage",
@@ -59,7 +61,7 @@ FEED_STORAGES = {
 
 # Uncomment the FEED_URI for whichever provider you're using
 
-# FEED_URI = "s3://{bucket}/%(year)s/%(month)s/%(day)s/%(hour_min)s/%(name)s.json".format(
+# FEED_URI = "s3://{bucket}/%(year)s/%(month)s/%(day)s/%(hour_min)s/%(name)s.json".format(  # noqa
 #     bucket=S3_BUCKET
 # )
 
